6e5e6a51d16df9cc1aa2d66775a19c0fa8508600
Alex <inc238@gmail.com>
2018-02-24 16:03:39 +0700

    update save client informations№2

--- app.rb
+++ app.rb
@@ -27,48 +27,7 @@ get '/visit' do
 end
 
 post '/visit' do
-    #@user_name = params[:user_name]
-    #@phone = params[:phone]
-    #@auto = params[:auto]
-    #@data = params[:data]
-    #@color = params[:color]
-
-    #if @color=="#7bd148"
-    #    @color='Green'
-    #elsif @color=="#5484ed"
-    #      @color = 'Bold blue'
-    #elsif @color=="#5484ed"
-    #      @color = 'Bold blue'
-    #elsif @color=="a4bdfc"
-    #      @color = 'Blue'
-    #elsif @color=="#46d6db"
-    #      @color = 'Turquoise'
-    #elsif @color=="#7ae7bf"
-    #      @color = 'Light green'
-    #elsif @color=="#51b749"
-    #      @color = 'Bold green'
-    #elsif @color=="#fbd75b"
-    #      @color = 'Yellow'
-    #elsif @color=="#ffb878"
-    #      @color = 'Orange'
-    #elsif @color=="#ff887c"
-    #      @color = 'Red'
-    #elsif @color=="#dc2127"
-    #      @color = 'Bold red'
-    #elsif @color=="#dbadff"
-    #      @color = 'Purple'
-    #elsif @color=="#e1e1e1"
-    #      @color = 'Gray'
-    #end
  c = Client.new params[:client]
-  #  client = Client.new # Сохранение в БД
-  #    client.name = @user_name
-  #    client.phone = @phone
-  #    client.datestamp = @data
-  #    client.color = @color
-  #    client.auto = @auto
-
-  #  client.save
 c.save
     erb "<h2> Спасибо, Вы записались!</h2> "
 end
